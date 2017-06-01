fcdf7f1e1a914edc26aa68473cb0d5be78bbce9a
Andrew Jones <jones.gaffney1920@gmail.com>
2017-06-01 17:50:14 -0400

    Prep for Heroku pt 2

--- src/twilioTime/TwilioServlet.java
+++ src/twilioTime/TwilioServlet.java
@@ -1,6 +1,5 @@
 package twilioTime;
 
-/*
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,21 +12,23 @@ import com.twilio.twiml.TwiMLException;
 
 public class TwilioServlet extends HttpServlet {
 
-  // service() responds to both GET and POST requests.
-  // You can also use doGet() or doPost()
-  public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Message sms =
-      new Message.Builder().body(new Body("The Robots are coming! Head for the hills!")).build();
+    // service() responds to both GET and POST requests.
+    // You can also use doGet() or doPost()
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Message message = new Message.Builder()
+                .body(new Body("Hello, Mobile Monkey"))
+                .build();
 
-    MessagingResponse twiml = new MessagingResponse.Builder().message(sms).build();
+        MessagingResponse twiml = new MessagingResponse.Builder()
+                .message(message)
+                .build();
 
-    response.setContentType("application/xml");
+        response.setContentType("web.xml");
 
-    try {
-      response.getWriter().print(twiml.toXml());
-    } catch (TwiMLException e) {
-      e.printStackTrace();
+        try {
+            response.getWriter().print(twiml.toXml());
+        } catch (TwiMLException e) {
+            e.printStackTrace();
+        }
     }
-  }
 }
-*/
